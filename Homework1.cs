92f28c41c75316e977e3dcd5b4930a0d002f2113
Justin Black <122068312+jleeblack@users.noreply.github.com>
2023-01-20 20:03:12 -0600

    Homework1 code file

---
+++ Homework1.cs
@@ -0,0 +1,12 @@
+﻿namespace Homework1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //the code for Homework1
+        double x = 2.5;
+        double y = 3.3;
+        double z = (4*x*x);
+        Console.WriteLine(z + (3*y));
+    }
+}
